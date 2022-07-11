a8c64cb1b12461b562cf755e66acc39089b82595
oopsmails <oopsmails@gmail.com>
2022-07-11 15:16:13 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# gradle-tutorial
+
+- Ref
+
+https://www.youtube.com/watch?v=-dtcEMLNmn0
+
+## Learning
+
+
+
---
+++ src/test/java/com/oopsmails/simplemain/SimpleMainTest.java
@@ -0,0 +1,10 @@
+package com.oopsmails.simplemain;
+
+import org.junit.Test;
+
+public class SimpleMainTest {
+	@Test
+	public void verifyNoExceptionThrown() {
+		SimpleMain.main(new String[] {});
+	}
+}
